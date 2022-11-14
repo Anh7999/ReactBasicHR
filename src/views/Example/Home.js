2a25e1302507b85b8ef4ae0e462f5eb436692d6b
Nguyen Trung Anh <anhtrung.nguyen@dxc.com>
2022-11-14 09:04:16 +0700

    redux

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,17 +1,64 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import logo from "../../assets/images/logo-react-svgrepo-com.svg";
+import { connect } from "react-redux";
 class Home extends React.Component {
   componentDidMount() {
     // setTimeout(() => {
     //   this.props.history.push("/todo");
     // }, 3000);
   }
+  hanldeDeleteUser = (user) => {
+    console.log(user);
+    this.props.deleteuserRedux(user);
+  };
+  hanldeCreateUser = () => {
+    this.props.addUserRredux();
+  };
   render() {
-    console.log(">>>>check prop: ", this.props);
-    return <div>Hello Trung Anh React</div>;
+    console.log(">>>>check prop: ", this.props.dataRedux);
+    let listUsers = this.props.dataRedux;
+    return (
+      <>
+        <div>Hello Trung Anh React</div>
+        <div>
+          <img
+            src={logo}
+            style={{ width: "200px", height: "200px", marginTop: "20px" }}
+          />
+        </div>
+        <div>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name} &nbsp;{" "}
+                  <span onClick={() => this.hanldeDeleteUser(item)}>X</span>
+                </div>
+              );
+            })}
+        </div>
+        <button onClick={() => this.hanldeCreateUser()}>Add New</button>
+      </>
+    );
   }
 }
 // export default withRouter(Home);
-export default Color(Home);
 
+const mapStateToProps = (state) => {
+  return {
+    dataRedux: state.users,
+  };
+};
+
+const maoDispatchToProps = (dispatch) => {
+  return {
+    deleteuserRedux: (userDelete) =>
+      dispatch({ type: "DELETE_USER", payload: userDelete }),
+    addUserRredux: () => dispatch({ type: "CREATE_USER" }),
+  };
+};
+
+export default connect(mapStateToProps, maoDispatchToProps)(Color(Home));
